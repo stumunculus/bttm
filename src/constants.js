fc0465fcf58304d829989cfb53db83d71773d24c
stuPorell <stuporell@gmail.com>
2024-08-15 23:38:47 -0400

    Added game phase to constants, updated hard_out to change game phase, started StrategyPhasePage

--- src/constants.js
+++ src/constants.js
@@ -2,34 +2,42 @@ export const STRATEGY_CARDS = [
   {
     number: 1,
     name: "Leadership",
+    color: "red",
   },
   {
     number: 2,
     name: "Diplomacy",
+    color: "orange",
   },
   {
     number: 3,
     name: "Politics",
+    color: "yellow",
   },
   {
     number: 4,
     name: "Construction",
+    color: "green",
   },
   {
     number: 5,
     name: "Trade",
+    color: "aqua",
   },
   {
     number: 6,
     name: "Warfare",
+    color: "blue",
   },
   {
     number: 7,
     name: "Technology",
+    color: "navy",
   },
   {
     number: 8,
     name: "Imperial",
+    color: "purple",
   },
 ];
 
@@ -38,5 +46,14 @@ export const APP_MODES = {
   game: "GAME_MODE",
 };
 
+export const gamePhases = {
+  strategy: "strategy",
+  action: "action",
+  status: "status",
+  agenda: "agenda",
+};
+
 export const MIN_PLAYERS = 3;
 export const MAX_PLAYERS = 8;
+export const numOfStrats = (players) =>
+  STRATEGY_CARDS.length - (STRATEGY_CARDS.length % players);
---
+++ src/pages/StrategyPhasePage.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import { STRATEGY_CARDS } from "../constants";
+
+export default function StrategyPhasePage() {
+  const [availStratCards, setAvailStratCards] = useState(STRATEGY_CARDS);
+  const renderStrategy = (strat) => {};
+}
